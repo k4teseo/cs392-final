54a3c3ed94d42b7beaeebb59e20a64a076506519
Kate Seo <kateseo@bu.edu>
2024-08-05 08:32:44 -0400

    updating API

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,21 +1,31 @@
 import styled from "styled-components";
 
 const StyledDiv = styled.div`
+  display: flex;
+`
+
+const Container = styled.div`
   background-color: white;
   box-shadow: 0 0 20px 0 rgba(0,0,0,.2);
+  width: 18vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 2%;
 `;
 
 const StyledImg = styled.img`
-    width: 150px;
+    width: 200px;
 `;
 
 function Book(book) {
     return(
         <StyledDiv>
+        <Container>
             <h1>{book.title}</h1>
             <StyledImg src={book.cover}/>
             <h2>{book.author}</h2>
-            <p>{book.description}</p>
+        </Container>
         </StyledDiv>
     )
 }
